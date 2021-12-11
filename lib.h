1ca55e1fa9b9dd068e4ece50557cde3c5a97e0de
GiuliaBalduzzi <91681343+GiuliaBalduzzi@users.noreply.github.com>
2021-12-11 14:47:29 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,10 @@
+//
+// Created by ICM on 11/12/2021.
+//
 
+#ifndef ESERCITAZIONE_8_LIB_H
+#define ESERCITAZIONE_8_LIB_H
+
+float numeroMassimo(const float numeri[], int dimensione);
+
+#endif //ESERCITAZIONE_8_LIB_H
