d4598d60390e0a9c0d8634cc801261848e06b814
GiuliaBalduzzi <91681343+GiuliaBalduzzi@users.noreply.github.com>
2021-12-11 14:48:05 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,13 @@
+//
+// Created by ICM on 11/12/2021.
+//
 
+#include "lib.h"
+float numeroMassimo(const float numeri [], int dim){
+    numeri[dim];
+    float massimo = numeri [0];
+    for (int i=0; i<dim; i++){
+        if (massimo < numeri[i]){
+            massimo = numeri [i];
+        }
+    }
